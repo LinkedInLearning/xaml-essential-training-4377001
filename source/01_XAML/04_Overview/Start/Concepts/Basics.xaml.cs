23573788a3a31abf567afc1af55b694a7b12de34
Walt Ritscher <waltritscher@outlook.com>
2023-02-12 15:55:31 -0800

    Add Overview project

---
+++ source/01_XAML/04_Overview/Start/Concepts/Basics.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Concepts {
+	/// <summary>
+	/// Interaction logic for Basics.xaml
+	/// </summary>
+	public partial class Basics : Window {
+		public Basics() {
+			InitializeComponent();
+
+// C# code
+var stack = new StackPanel();
+var titleText = new TextBlock();
+titleText.Text = "Tour Stops";
+
+var kids = new CheckBox();
+kids.IsChecked = true;
+kids.Content = "Kid Friendly";
+
+stack.Children.Add(titleText);
+stack.Children.Add(kids);
+
+
+		}
+	}
+}
---
+++ source/01_XAML/04_Overview/Start/Concepts/ExampleWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Concepts {
+  /// <summary>
+  /// Interaction logic for ExampleWindow.xaml
+  /// </summary>
+  public partial class ExampleWindow : Window {
+    public ExampleWindow() {
+      InitializeComponent();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+
+    }
+  }
+}
