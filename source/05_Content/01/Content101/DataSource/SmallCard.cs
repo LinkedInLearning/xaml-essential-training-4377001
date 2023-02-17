6f58954fc595065eb0b5eaa3cb1b3b01a3865cf2
Walt Ritscher <waltritscher@outlook.com>
2023-02-17 11:36:59 -0800

    Remove models folder

---
+++ source/05_Content/01/Content101/DataSource/SmallCard.cs
@@ -0,0 +1,19 @@
+﻿using BigStar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content101.DataSource {
+	internal class SmallCard {
+		public int Id { get; set; }
+		public string CardName { get; set; }
+		public decimal Price { get; set; }
+		public TeamNames Team { get; set; }
+
+		public override string ToString() {
+			return $"Card: {CardName}, {Team}, {Price:C}";
+		}
+	}
+}
