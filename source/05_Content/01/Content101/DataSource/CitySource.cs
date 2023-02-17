157a688585813fe4760514e97a8245028dc3f53e
Walt Ritscher <waltritscher@outlook.com>
2023-02-17 01:09:39 -0800

    Add class diagrams

---
+++ source/05_Content/01/Content101/DataSource/CitySource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content101.DataSource {
+	public class CitySource {
+
+		private List<string> _cities;
+		public CitySource() {
+			_cities = new List<string> { "Barcelona", "São Paulo", "Singapore", "Bangkok" };
+		}
+		public List<string> Cities
+		{
+			get
+			{
+				return _cities;
+			}
+		}
+
+	}
+}
