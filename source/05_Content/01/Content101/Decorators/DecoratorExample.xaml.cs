af62e0a7f00ee6f44289007367c51c1ff9c13b14
Walt Ritscher <waltritscher@outlook.com>
2023-02-17 00:05:58 -0800

    Add decorator example

---
+++ source/05_Content/01/Content101/Decorators/DecoratorExample.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Content101.Decorators {
+	/// <summary>
+	/// Interaction logic for DecoratorExample.xaml
+	/// </summary>
+	public partial class DecoratorExample : Window {
+		public DecoratorExample() {
+			InitializeComponent();
+		}
+	}
+}
