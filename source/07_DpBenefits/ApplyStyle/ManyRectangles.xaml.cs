15a4f0030a644ba20c0a185d57aa7e60564c0a20
Walt Ritscher <waltritscher@outlook.com>
2023-02-20 22:51:34 -0800

    Add styles example

--- source/07_DpBenefits/ApplyStyle/ManyRectangles.xaml.cs
+++ source/07_DpBenefits/ApplyStyle/ManyRectangles.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PropInheritance {
+namespace ApplyStyle {
 	/// <summary>
 	/// Interaction logic for ManyRectangles.xaml
 	/// </summary>
---
+++ source/07_DpBenefits/ApplyStyle/ManyStars.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ApplyStyle {
+    /// <summary>
+    /// Interaction logic for ManyStars.xaml
+    /// </summary>
+    public partial class ManyStars : Window
+    {
+        public ManyStars()
+        {
+            InitializeComponent();
+        }
+    }
+}
