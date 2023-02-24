a35a5ac3bd139ce33127d7d18b7c68d075cd5f68
Walt Ritscher <waltritscher@outlook.com>
2023-02-23 16:07:13 -0800

    Add markup extension example

---
+++ source/10_MarkupExt/CustomMarkupExt/AddProperties/RelativeColor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace AddProperties {
+
+	[MarkupExtensionReturnType(typeof(Brush))]
+	internal class RelativeColor : MarkupExtension {
+		private Color _color = Colors.Orange;
+		private ColorAction _colorAction = ColorAction.Normal;
+
+		public RelativeColor() { }
+
+		public Color Color
+		{
+			get { return _color; }
+			set { _color = value; }
+		}
+
+		public ColorAction ColorAction
+		{
+			get { return _colorAction; }
+			set { _colorAction = value; }
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider) {
+			var newColor = _colorAction switch
+			{
+				ColorAction.Lighten => new SolidColorBrush(LightenColor(_color)),
+				ColorAction.Darken => new SolidColorBrush(DarkenColor(_color)),
+				ColorAction.Normal => new SolidColorBrush(_color),
+				_ => new SolidColorBrush(_color)
+			};
+			return newColor;
+		}
+
+		private Color LightenColor(Color currentColor) {
+			var hsl = new ColorLib.HslColor(currentColor);
+			return hsl.Lighten(.3).ToRgb();
+		}
+
+		private Color DarkenColor(Color currentColor) {
+			var hsl = new ColorLib.HslColor(currentColor);
+			return hsl.Darken(.2).ToRgb();
+		}
+	}
+
+	public enum ColorAction {
+		Normal,
+		Lighten,
+		Darken
+	}
+}
---
+++ source/10_MarkupExt/CustomMarkupExt/CustomMarkupExt/RelativeColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace CustomMarkupExt {
+
+	[MarkupExtensionReturnType(typeof(Brush))]
+	internal class RelativeColor : MarkupExtension {
+		private Color _color = Colors.Orange;
+
+		// use a hard coded color
+		public RelativeColor() {
+		}
+		//public Color Color
+		//{
+		//	get { return _color; }
+		//	set { _color = value; }
+		//}
+		public override object ProvideValue(IServiceProvider serviceProvider) {
+			return new SolidColorBrush(_color);
+		}
+	}
+}
