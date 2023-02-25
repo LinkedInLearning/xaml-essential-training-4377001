5248940fd0097198230c908f80076a14512b197d
Walt Ritscher <waltritscher@outlook.com>
2023-02-25 00:45:54 -0800

    Add more resource examples

---
+++ source/11_Resources/CommonResources/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommonResources {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+		private void DataButton_Click(object sender, RoutedEventArgs e) {
+			(new DataWindow()).Show();
+		}
+
+		private void StyleButton_Click(object sender, RoutedEventArgs e) {
+			(new StyleWindow()).Show();
+		}
+
+		private void ColorButton_Click(object sender, RoutedEventArgs e) {
+			(new ColorWindow()).Show();
+		}
+
+	}
+}
