4f8dc42cf7efa9ea3d2035c6c8229cb2a251c2f4
Walt Ritscher <waltritscher@outlook.com>
2023-02-25 00:19:41 -0800

    Add the resources example solution

---
+++ source/11_Resources/Where/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WhereResource {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+
+      var dictionary = new ResourceDictionary();
+
+      dictionary.Add(key: "MainBrush", value: new SolidColorBrush(Colors.Lavender));
+      dictionary.Add(key: "AccentBrush", value: new SolidColorBrush(Colors.Gold));
+
+
+
+      this.Resources = dictionary;
+
+
+    }
+  }
+}
