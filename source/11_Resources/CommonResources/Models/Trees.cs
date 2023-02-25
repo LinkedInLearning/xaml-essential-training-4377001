9da0c6ec39f218cd1a73a666249c4149c349f09f
Walt Ritscher <waltritscher@outlook.com>
2023-02-25 01:00:25 -0800

    Add merged resources examples

---
+++ source/11_Resources/CommonResources/Models/Trees.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Models {
+
+  internal class Trees : ObservableCollection<Tree> {
+
+    public Trees() {
+      this.Add(new Tree { TreeName = "Fir", MaxHeight = 90 });
+      this.Add(new Tree { TreeName = "Oak", MaxHeight = 60 });
+      this.Add(new Tree { TreeName = "Pine", MaxHeight = 85 });
+      this.Add(new Tree { TreeName = "Palm", MaxHeight = 25 });
+      this.Add(new Tree { TreeName = "Cedar", MaxHeight = 95 });
+
+      
+    }
+
+    
+  }
+  internal class Tree {
+    public string TreeName { get; set; }
+    public int MaxHeight { get; set; }
+  }
+}
